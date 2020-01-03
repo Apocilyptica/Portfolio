297d9172c4b2c43949759e171adec2c71f096863
Apocilyptica <57518472+Apocilyptica@users.noreply.github.com>
2020-01-03 18:31:14 -0500

    content update

---
+++ styles/maincontent.js
@@ -0,0 +1,84 @@
+function homeFunction() {
+    var a = document.getElementById("homeContent");
+    var b = document.getElementById("aboutmeContent");
+    var c = document.getElementById("educationContent");
+    var d = document.getElementById("portfolioContent");
+    var e = document.getElementById("contactContent");
+    if (a.style.display == "none" || b.style.display === "block" || c.style.display === "block" || d.style.display === "block" || e.style.display === "block") {
+      a.style.display = "block";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+      e.style.display = "none";
+    } else {
+      a.style.display = "none";
+    }
+  }
+
+function aboutmeFunction() {
+    var a = document.getElementById("homeContent");
+    var b = document.getElementById("aboutmeContent");
+    var c = document.getElementById("educationContent");
+    var d = document.getElementById("portfolioContent");
+    var e = document.getElementById("contactContent");
+    if (a.style.display == "block" || b.style.display === "none" || c.style.display === "block" || d.style.display === "block" || e.style.display === "block") {
+        a.style.display = "none";
+        b.style.display = "block";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+    } else {
+      b.style.display = "none";
+    }
+  }
+
+function educationFunction() {
+    var a = document.getElementById("homeContent");
+    var b = document.getElementById("aboutmeContent");
+    var c = document.getElementById("educationContent");
+    var d = document.getElementById("portfolioContent");
+    var e = document.getElementById("contactContent");
+    if (a.style.display == "block" || b.style.display === "block" || c.style.display === "none" || d.style.display === "block" || e.style.display === "block") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "block";
+        d.style.display = "none";
+        e.style.display = "none";
+    } else {
+      c.style.display = "none";
+    }
+  }
+
+function portfolioFunction() {
+    var a = document.getElementById("homeContent");
+    var b = document.getElementById("aboutmeContent");
+    var c = document.getElementById("educationContent");
+    var d = document.getElementById("portfolioContent");
+    var e = document.getElementById("contactContent");
+    if (a.style.display == "block" || b.style.display === "block" || c.style.display === "block" || d.style.display === "none" || e.style.display === "block") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "block";
+        e.style.display = "none";
+    } else {
+      d.style.display = "none";
+    }
+  }
+
+function contactFunction() {
+    var a = document.getElementById("homeContent");
+    var b = document.getElementById("aboutmeContent");
+    var c = document.getElementById("educationContent");
+    var d = document.getElementById("portfolioContent");
+    var e = document.getElementById("contactContent");
+    if (a.style.display == "block" || b.style.display === "block" || c.style.display === "block" || d.style.display === "block" || e.style.display === "none") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "block";
+    } else {
+      e.style.display = "none";
+    }
+  }
